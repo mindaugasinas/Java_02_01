df247520c3ee35c7aa0fce78f792c73665b09249
mindaugasinas <sinkeviciusm@gmail.com>
2018-01-30 14:38:21 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Iveskite savo varda");
+
+        String Vardas = sc.nextLine();
+
+
+        System.out.println(Vardas);
+        System.out.println(Vardas);
+        System.out.println(Vardas);
 	// write your code here
     }
 }
